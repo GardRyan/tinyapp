3c0661b0a99eb038b7764813ac4dc9cb823059f4
GardRyan <res.gardner@gmail.com>
2024-01-23 16:37:05 -0500

    debugging

--- express_server.js
+++ express_server.js
@@ -36,10 +36,7 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
-
-    
+    const longURL = req.body.longURL;
     const shortURL = generateRandomString();
     urlDatabase[shortURL] = longURL;
     
