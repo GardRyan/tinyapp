bf8649558ea5589aa9c4fd30a2cbedaead578a56
GardRyan <res.gardner@gmail.com>
2024-01-23 21:01:43 -0500

    implemented delete button

--- express_server.js
+++ express_server.js
@@ -44,6 +44,13 @@ app.post("/urls", (req, res) => {
 
 });
 
+app.post("/urls/:id/delete", (req, res) => {
+  const id = req.params.id;  
+  delete urlDatabase[id];
+    
+  res.redirect("/urls");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
